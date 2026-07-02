a4a81317fba62b1ff19f4c0082ea0971930d9f5f
biostehno <biostehno@gmail.com>
2026-07-02 18:56:30 +0300

    Cats

--- My-App/App/Main/src/Main.java
+++ My-App/App/Main/src/Main.java
@@ -1,7 +1,7 @@
 package App;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Shop-online for customers");//комментарий
+        System.out.println("Shop-online for customergit and cats");//комментарий
 
     }
 }
